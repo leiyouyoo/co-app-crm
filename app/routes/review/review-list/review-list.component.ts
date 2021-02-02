0d34d1136beff78f95a90e2b56c966fd06b1ac63
penglin <519645581@qq.com>
2021-02-02 09:23:40 +0800

    fix

--- app/routes/review/review-list/review-list.component.ts
+++ app/routes/review/review-list/review-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, Injector, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { STChange, STColumn, STComponent, STData, STRequestOptions } from '@co/cbc';
-import { CoPageBase } from '@co/core';
+import { CoPageBase, debounce } from '@co/core';
 import { CRMCustomerSolutionService } from 'apps/crm/app/services/crm';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { SSOUserService } from '@co/cds';
 import { ReviewAgentType, ReviewBusinessType, ReviewStatusType } from './../enums';
+import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'crm-review-list',
   templateUrl: './review-list.component.html',
@@ -21,8 +22,14 @@ export class ReviewListComponent extends CoPageBase {
   serviceType = 1;
   reqParams = {
     businessNo: '',
+    skipCount: 1,
     maxResultCount: 20,
+    creatorUserId: null as any,
+    agentType: null,
   };
+  userList: any;
+  loading = false;
+
   menuLoading = false;
   selectedMenuIndex = 0;
   menuOfList = [
@@ -77,12 +84,15 @@ export class ReviewListComponent extends CoPageBase {
     { title: '回复人', index: 'replierUser' },
     { title: '回复日期', index: 'replyTime' },
     {
+      title: 'Action',
       type: 'action',
-      width: 120,
+      width: 150,
       fixed: 'right',
+      className: 'no-line-through',
       buttons: [
         {
-          text: '回复',
+          text: this.$L('View'),
+          type: 'none',
           click: (e) => {},
         },
       ],
@@ -111,10 +121,14 @@ export class ReviewListComponent extends CoPageBase {
   }
 
   coOnInit(): void {
-    this.customerSolutionService.getApplyList(this.reqParams).subscribe((res) => {
-      this.listOfData = res.items;
-      this.totalCount = res.totalCount;
-    });
+    this.getUsers();
+    this.getList();
+  }
+
+  keys(): Array<string> {
+    var data = ReviewBusinessType;
+    var keys = Object.keys(data);
+    return keys.slice(keys.length / 2);
   }
 
   coOnActived(): void {}
@@ -150,7 +164,10 @@ export class ReviewListComponent extends CoPageBase {
   resetParam() {
     this.reqParams = {
       businessNo: '',
+      skipCount: 1,
       maxResultCount: 20,
+      creatorUserId: null as any,
+      agentType: null,
     };
   }
 
@@ -163,12 +180,61 @@ export class ReviewListComponent extends CoPageBase {
         this.goDetail(e.dblClick.item);
         break;
       case 'checkbox':
-
+        break;
+      case 'pi':
+        this.pageIndexChange(e.pi);
+        break;
+      case 'ps':
+        this.pageSizeChange(e.ps);
+        break;
       default:
     }
   }
 
+  pageIndexChange(event: number): void {
+    this.reqParams.skipCount = event;
+    this.getList();
+  }
+
+  pageSizeChange(event: number): void {
+    this.reqParams.maxResultCount = event;
+    this.getList();
+  }
+
   goDetail(e) {}
+
+  getList() {
+    let num = this.reqParams.skipCount - 1;
+    this.loading = true;
+    this.customerSolutionService
+      .getApplyList({
+        maxResultCount: this.reqParams.maxResultCount,
+        skipCount: num * this.reqParams.maxResultCount,
+        businessNo: this.reqParams.businessNo,
+        creatorUserId: this.reqParams.creatorUserId,
+        agentType: (this.reqParams.agentType ? ReviewAgentType[this.reqParams.agentType] : null) as any,
+      })
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe((res) => {
+        this.listOfData = res.items;
+        this.totalCount = res.totalCount;
+      });
+  }
+
+  getUsers(data = null) {
+    this.ssoUserService
+      .getCityOceanUsers({
+        name: data,
+      })
+      .subscribe((res: any) => {
+        this.userList = res.items;
+      });
+  }
+
+  @debounce(200)
+  searchData(data) {
+    this.getUsers(data);
+  }
 
   onMenuListClick(data, i) {}
   //#endregion
