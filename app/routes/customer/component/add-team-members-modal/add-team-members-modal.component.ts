ed202b0ed4c395c369c60d7ba9ba336a133730ca
MinoMunMun <wunonglin@gmail.com>
2021-03-30 18:29:06 +0800

    完成团队成员

--- app/routes/customer/component/add-team-members-modal/add-team-members-modal.component.ts
+++ app/routes/customer/component/add-team-members-modal/add-team-members-modal.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { _HttpClient } from '@co/common';
-import { CRMCustomerAccessAllowService } from 'apps/crm/app/services/crm';
+import {
+  CRMCreateOrUpdateAccessAllowInput,
+  CRMCustomerAccessAllowOutput,
+  CRMCustomerAccessAllowService,
+} from 'apps/crm/app/services/crm';
 // tslint:disable-next-line:import-blacklist
 import { NzModalRef } from 'ng-zorro-antd';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { shareReplay, tap } from 'rxjs/operators';
+import { BehaviorSubject, forkJoin, Observable } from 'rxjs';
+import { map, shareReplay, tap } from 'rxjs/operators';
 
-interface Customer {
+interface User {
   creationTime: string;
   creator: string;
   customerId: string;
@@ -34,8 +38,10 @@ interface Customer {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddTeamMembersModalComponent implements OnInit {
-  selected: string[] = [];
-  customers: Observable<Customer[]>;
+  @Input() customerId: string;
+  @Input() users: CRMCustomerAccessAllowOutput[];
+  selected: number[] = [];
+  userList: Observable<User[]>;
   customersLoading = new BehaviorSubject<boolean>(true);
   adding = new BehaviorSubject<boolean>(false);
 
@@ -44,7 +50,7 @@ export class AddTeamMembersModalComponent implements OnInit {
     private crmCustomerAccessAllowService: CRMCustomerAccessAllowService,
     private nzModalRef: NzModalRef<AddTeamMembersModalComponent, boolean>
   ) {
-    this.customers = this.http.get<Customer[]>('/SSO/User/GetAllActiveUserBySearch').pipe(
+    this.userList = this.http.get<User[]>('/SSO/User/GetAllActiveUserBySearch').pipe(
       tap(() => this.customersLoading.next(false)),
       shareReplay({
         bufferSize: 1,
@@ -54,14 +60,62 @@ export class AddTeamMembersModalComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    /** 复原勾选的id */
+    this.selected = this.users.map(i => i.allowUserId);
   }
 
   close(): void{
     this.nzModalRef.close();
   }
 
+  getDeleteUserId(): number[] {
+    const arr: number[] = [];
+
+    for (const item of this.users){
+      if (!this.selected.includes(item.allowUserId)){
+        arr.push(item.allowUserId);
+      }
+    }
+
+    return arr;
+  }
+
+  getAddUserId(): number[] {
+    const ids: number[] = this.users.map(i => i.allowUserId);
+    const arr: number[] = [];
+
+    for (const item of this.selected){
+      if (!ids.includes(item)){
+        arr.push(item);
+      }
+    }
+
+    return arr;
+  }
+
+  /**
+   * 保存说明
+   * 1、根据users获得取消勾选的id
+   * 2、根据users去除已在系统的数据
+   * 3、获得新增的id
+   * 4、使用forkJoin将删除和新增请求同时发送
+   * 5、根据结果关闭弹窗
+   */
   save(): void{
-    this.crmCustomerAccessAllowService.create({}).subscribe(() => {
+    forkJoin([
+      this.crmCustomerAccessAllowService.create(this.selected.map(item => {
+        const data: CRMCreateOrUpdateAccessAllowInput = {
+          customerId: this.customerId,
+          allowUserId: item
+        };
+
+        return data;
+      })),
+      this.crmCustomerAccessAllowService.delete({
+        customerId: this.customerId,
+        accessAllowUserIds: this.getDeleteUserId()
+      })
+    ]).subscribe(() => {
       this.nzModalRef.close(true);
     });
   }
--- app/routes/customer/component/team-members/team-members.component.ts
+++ app/routes/customer/component/team-members/team-members.component.ts
@@ -4,8 +4,8 @@ import { FormControl } from '@angular/forms';
 import { CRMCustomerAccessAllowOutput, CRMCustomerAccessAllowService } from 'apps/crm/app/services/crm';
 // tslint:disable-next-line:import-blacklist
 import { NzModalService } from 'ng-zorro-antd';
-import { merge, Observable, of } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map, shareReplay, switchMap } from 'rxjs/operators';
+import { merge, Observable, of, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, shareReplay, switchMap, tap } from 'rxjs/operators';
 import { AddTeamMembersModalComponent } from '../add-team-members-modal/add-team-members-modal.component';
 
 @Component({
@@ -19,20 +19,23 @@ export class TeamMembersComponent implements OnInit {
   selected = new SelectionModel<string>(true);
   keyword = new FormControl('');
   members: Observable<CRMCustomerAccessAllowOutput[]>;
+  reload$ = new Subject<void>();
 
   constructor(
     private crmCustomerAccessAllowService: CRMCustomerAccessAllowService,
     private nzModalService: NzModalService
   ) {
     this.members = merge(
-      of(''),
+      of(null),
       this.keyword.valueChanges.pipe(
         debounceTime(300),
         distinctUntilChanged()
-      )
+      ),
+      this.reload$
     ).pipe(
-      switchMap((value: string) => this.crmCustomerAccessAllowService.getAll({
-        customerId: this.customerId
+      switchMap((value: string | undefined | null) => this.crmCustomerAccessAllowService.getAll({
+        customerId: this.customerId,
+        name: value
       })),
       map(result => result.items),
       shareReplay({
@@ -46,32 +49,31 @@ export class TeamMembersComponent implements OnInit {
   }
 
   openRemoveModal(): void{
-    // this.nzModalService.confirm({
-    //   nzTitle: '确认删除?',
-    //   nzOnOk: () => this.crmCustomerAccessAllowService.delete({
-    //     customerId: this.customerId,
-    //     id: this.selected.selected
-    //   }).toPromise()
-    // });
+    this.nzModalService.confirm({
+      nzTitle: '确认删除?',
+      nzOnOk: () => this.crmCustomerAccessAllowService.delete({
+        customerId: this.customerId,
+        accessAllowUserIds: this.selected.selected
+      }).pipe(tap(() => this.reload$.next())).toPromise()
+    });
   }
 
-  openAddTeamMembersModal(): void{
+  openAddTeamMembersModal(items: CRMCustomerAccessAllowOutput[]): void{
     const modal = this.nzModalService.create<AddTeamMembersModalComponent, boolean | undefined | null>({
       nzTitle: '添加成员',
       nzContent: AddTeamMembersModalComponent,
       nzComponentParams: {
-
+        customerId: this.customerId,
+        users: items
       },
       nzWidth: 800,
       nzFooter: null
     });
 
-    modal.afterOpen.subscribe(result => {
-      console.log('afterOpen');
-    });
-
     modal.afterClose.subscribe(result => {
-      console.log(result);
+      if (result){
+        this.reload$.next();
+      }
     });
   }
 }
