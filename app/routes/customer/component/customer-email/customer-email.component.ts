7ab52990de49751ba0458e95dc7d3b21f5dceca9
MinoMunMun <wunonglin@gmail.com>
2021-03-30 10:24:11 +0800

    no message

--- app/routes/customer/component/customer-email/customer-email.component.ts
+++ app/routes/customer/component/customer-email/customer-email.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'crm-customer-email',
   templateUrl: './customer-email.component.html',
-  styleUrls: ['./customer-email.component.less']
+  styleUrls: ['./customer-email.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CustomerEmailComponent implements OnInit {
+  @Input() customerId: string;
 
   constructor() { }
 
