ab40b60abc11e95a94e5a49374e37d71da52f043
MinoMunMun <wunonglin@gmail.com>
2021-04-01 14:56:11 +0800

    完成邮件功能

--- app/routes/customer/component/customer-email/customer-email.component.ts
+++ app/routes/customer/component/customer-email/customer-email.component.ts
@@ -1,12 +1,24 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { _HttpClient } from '@co/common';
 import { CRMContactService, CRMEmailService, CRMGetAllSalesAndContactsOutput } from 'apps/crm/app/services/crm';
+import { STORAGEFileService } from 'apps/crm/app/services/storage';
 import { CKEditor4 } from 'ckeditor4-angular';
-// tslint:disable-next-line:import-blacklist
-import { NzModalService } from 'ng-zorro-antd';
-import { BehaviorSubject, EMPTY, merge, Observable, of, Subject } from 'rxjs';
-import { catchError, debounceTime, distinctUntilChanged, map, shareReplay, switchMap, take, takeUntil, tap } from 'rxjs/operators';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { BehaviorSubject, EMPTY, from, merge, Observable, of, Subject } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  mergeAll, retry,
+  shareReplay,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs/operators';
 import { EmailPasswordInputModalComponent } from './email-password-input-modal/email-password-input-modal.component';
 
 interface UserDetail {
@@ -22,6 +34,30 @@ interface UserDetail {
   userName: string;
 }
 
+export interface SelectedFile {
+  index: number;
+  data: File;
+}
+
+export interface FileInfo {
+  fileId: string;
+  url: string;
+  fileName: string;
+  extensionName: string;
+}
+
+export interface InitData{
+  customerId: string;
+  sender: UserDetail;
+  ccModel: boolean;
+  bccModel: boolean;
+  cc: string[];
+  bcc: string[];
+  subject: string;
+  value: string;
+  fileList: FileInfo[];
+}
+
 @Component({
   selector: 'crm-customer-email',
   templateUrl: './customer-email.component.html',
@@ -29,12 +65,12 @@ interface UserDetail {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomerEmailComponent implements OnInit, OnDestroy {
-  @Input() customerId: string;
-  @Input() private initSender: string;
+  @Input() customerId: string | undefined;
+  @Input() init: InitData | undefined;
+  @Input() fullScreenMode = false;
 
-  value = '';
   user: Observable<UserDetail>;
-  sender: Observable<string>;
+  sender: Observable<UserDetail>;
   receivesList: Observable<CRMGetAllSalesAndContactsOutput[]>;
   receivesKeyword = new Subject<string>();
   receivesSearchLoading = new BehaviorSubject<boolean>(false);
@@ -44,12 +80,14 @@ export class CustomerEmailComponent implements OnInit, OnDestroy {
   bcc: string[] = [];
   receives: string[] = [];
   password: string | undefined = undefined;
-
+  sending = new BehaviorSubject<boolean>(false);
   form = new FormGroup({
-    sender: new FormControl('', Validators.required),
     subject: new FormControl('', Validators.required),
     value: new FormControl('', Validators.required),
   });
+
+  selectedFile = new BehaviorSubject<SelectedFile[]>([]);
+  fileList = new BehaviorSubject<FileInfo[]>([]);
 
   readonly editorConfig: CKEditor4.Config = {};
 
@@ -59,7 +97,9 @@ export class CustomerEmailComponent implements OnInit, OnDestroy {
     private http: _HttpClient,
     private crmContactService: CRMContactService,
     private crmEmailService: CRMEmailService,
-    private nzModalService: NzModalService
+    private nzModalService: NzModalService,
+    private storageFileService: STORAGEFileService,
+    private nzMessageService: NzMessageService,
   ) {
     this.user = this.http.get<UserDetail>('/SSO/User/GetUserDetail').pipe(
       shareReplay({
@@ -68,41 +108,35 @@ export class CustomerEmailComponent implements OnInit, OnDestroy {
       }),
     );
 
-    this.sender = of(this.initSender).pipe(
+    this.sender = of(this.init?.sender).pipe(
       switchMap(initSender => {
         if (initSender) {
           return of(initSender);
         } else {
-          return this.user.pipe(
-            map(user => {
-              let value = '';
-
-              if (user.emailAddress) {
-                value = user.emailAddress;
-              }
-
-              return value;
-            }),
-          );
+          return this.user;
         }
+      }),
+      shareReplay({
+        bufferSize: 1,
+        refCount: true,
       })
     );
 
-    this.receivesList = merge(
-      of(null),
-      this.receivesKeyword.pipe(
-        distinctUntilChanged(),
-        debounceTime(600)
-      )
-    ).pipe(
+    this.receivesList = this.receivesKeyword.pipe(
+      distinctUntilChanged(),
+      debounceTime(600),
       switchMap((value: string | undefined | null) => {
-        this.receivesSearchLoading.next(true);
-        return this.crmContactService.getAllSalesAndContacts({
-          customerId: this.customerId,
-          searchText: value ? value : undefined
-        });
+        if (value && value.length > 0){
+          this.receivesSearchLoading.next(true);
+          return this.crmContactService.getAllSalesAndContacts({
+            customerId: this.customerId,
+            searchText: value ? value : undefined,
+            maxResultCount: 100
+          }).pipe(map(result => result.items));
+        }else {
+          return of([]);
+        }
       }),
-      map(result => result.items),
       tap(() => this.receivesSearchLoading.next(false)),
       shareReplay({
         bufferSize: 1,
@@ -112,8 +146,6 @@ export class CustomerEmailComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.form.controls.sender.disable({emitEvent: false});
-
     this.ccModel
       .pipe(takeUntil(this.unsubscribe))
       .subscribe(value => {
@@ -130,9 +162,17 @@ export class CustomerEmailComponent implements OnInit, OnDestroy {
         }
       });
 
-    this.sender.pipe(take(1)).subscribe(v => this.form.patchValue({
-      sender: v
-    }));
+    if (this.init){
+      this.cc = this.init.cc;
+      this.bcc = this.init.bcc;
+      this.form.patchValue({
+        subject: this.init.subject,
+        value: this.init.value
+      });
+      this.fileList.next(this.init.fileList);
+      this.ccModel.next(this.init.ccModel);
+      this.bccModel.next(this.init.bccModel);
+    }
   }
 
   ngOnDestroy() {
@@ -165,9 +205,105 @@ export class CustomerEmailComponent implements OnInit, OnDestroy {
     });
   }
 
-  openEmailPasswordInputModals(): Observable<string | undefined | null>{
+  uploadFile(event: Event): void{
+    const input = (event.target as HTMLInputElement);
+    const arr: SelectedFile[] = [];
+
+    for (let index = 0; index < input.files.length; index++){
+      const file = input.files.item(index);
+      if (file){
+        arr.push({
+          index,
+          data: file,
+        });
+      }
+    }
+
+    input.value = null;
+    this.selectedFile.next(arr);
+
+    from(arr).pipe(
+      map(item => {
+        return this.storageFileService.upload({
+          file: item.data
+        }).pipe(
+          retry(2),
+          tap(() => {
+            const local = this.selectedFile.getValue();
+            const index = local.findIndex(i => item.index === i.index);
+            local.splice(index, 1);
+            this.selectedFile.next(local);
+          })
+        );
+      }),
+      mergeAll(2),
+    ).subscribe({
+      next: result => {
+        this.fileList.next([...this.fileList.getValue(), {
+          fileId: result.fileId,
+          url: result.url,
+          fileName: result.fileName,
+          extensionName: result.extensionName
+        }]);
+      }
+    });
+  }
+
+  openFullScreen(): void{
+    this.sender.pipe(
+      switchMap(sender => {
+        const modal = this.nzModalService.create<CustomerEmailComponent, InitData>({
+          nzTitle: '邮件',
+          nzWidth: '90vw',
+          nzFooter: null,
+          nzContent: CustomerEmailComponent,
+          nzComponentParams: {
+            fullScreenMode: true,
+            init: {
+              customerId: this.customerId,
+              sender,
+              ccModel: this.ccModel.getValue(),
+              bccModel: this.bccModel.getValue(),
+              cc: this.cc,
+              bcc: this.bcc,
+              subject: this.form.value.subject,
+              value: this.form.value.value,
+              fileList: this.fileList.getValue()
+            }
+          },
+          nzOnCancel: instance => {
+            modal.afterClose.next({
+              customerId: instance.customerId,
+              sender,
+              ccModel: instance.ccModel.getValue(),
+              bccModel: instance.bccModel.getValue(),
+              cc: instance.cc,
+              bcc: instance.bcc,
+              subject: instance.form.value.subject,
+              value: instance.form.value.value,
+              fileList: instance.fileList.getValue()
+            });
+          }
+        });
+
+        return modal.afterClose;
+      })
+    ).subscribe(result => {
+      this.ccModel.next(result.ccModel);
+      this.bccModel.next(result.bccModel);
+      this.cc = result.cc;
+      this.bcc = result.bcc;
+      this.form.patchValue({
+        subject: result.subject,
+        value: result.value
+      });
+      this.fileList.next(result.fileList);
+    });
+  }
+
+  openEmailPasswordInputModals(title?: string): Observable<string | undefined | null>{
     return this.nzModalService.create<EmailPasswordInputModalComponent, string | undefined | null>({
-      nzTitle: '请输入密码',
+      nzTitle: title || '请输入密码',
       nzContent: EmailPasswordInputModalComponent,
       nzFooter: null
     }).afterClose;
@@ -177,48 +313,104 @@ export class CustomerEmailComponent implements OnInit, OnDestroy {
     return `${ item.fullName }<${ item.email }>`;
   }
 
+  removeFile(index: number): void{
+    const arr = this.fileList.getValue();
+    arr.splice(index, 1);
+    this.fileList.next(arr);
+  }
+
   submit(): void{
-    of(this.hasPassword()).pipe(
-      switchMap(value => {
-        /** 如果有密码那就直接发送邮件，不用带密码 */
-        if (value){
-          return this.crmEmailService.seed({
-            from: this.form.controls.sender.value,
-            to: this.receives,
-            subject: this.form.value.subject,
-            body: this.form.value.value,
-            cc: this.cc,
-            bcc: this.bcc,
-          });
-        }else {
-          /** 打开输入密码的弹窗 */
-          return this.openEmailPasswordInputModals().pipe(
-            switchMap(result => {
-              /** 如果填了密码，那就发送请求 */
-              if (result){
-                return this.crmEmailService.seed({
-                  from: this.form.controls.sender.value,
-                  to: this.receives,
-                  subject: this.form.value.subject,
-                  body: this.form.value.value,
-                  cc: this.cc,
-                  bcc: this.bcc,
-                  fromPassword: result
-                });
-              }else {
-                /** 没有的话那就直接完成这个observable结束操作 */
-                return EMPTY;
-              }
-            })
-          );
-        }
-      })
+    /** 检查有没有文件在上传中，等文件上传完成 */
+    if (this.selectedFile.getValue().length > 0){
+      this.nzMessageService.error('请等待文件上传完成', {
+        nzDuration: 1000
+      });
+    }
+
+    this.sending.next(true);
+    this.sender.pipe(
+      switchMap(sender => of(this.hasPassword()).pipe(
+        switchMap(password => {
+          /** 如果有密码那就直接发送邮件，不用带密码 */
+          if (password){
+            return this.crmEmailService.seed({
+              customerId: this.customerId,
+              from: sender.emailAddress,
+              fromUserId: sender.id,
+              to: this.receives,
+              subject: this.form.value.subject,
+              body: this.form.value.value,
+              cc: this.cc,
+              bcc: this.bcc,
+              attachments: this.fileList.getValue().map(i => i.fileId)
+            });
+          }else {
+            /** 打开输入密码的弹窗 */
+            return this.openEmailPasswordInputModals().pipe(
+              switchMap(result => {
+                /** 如果填了密码，那就发送请求 */
+                if (result){
+                  return this.crmEmailService.seed({
+                    customerId: this.customerId,
+                    from: sender.emailAddress,
+                    fromUserId: sender.id,
+                    to: this.receives,
+                    subject: this.form.value.subject,
+                    body: this.form.value.value,
+                    cc: this.cc,
+                    bcc: this.bcc,
+                    fromPassword: result,
+                    attachments: this.fileList.getValue().map(i => i.fileId)
+                  });
+                }else {
+                  /** 没有的话那就直接完成这个observable结束操作 */
+                  return EMPTY;
+                }
+              })
+            );
+          }
+        }),
+        catchError(err => {
+          if (err instanceof HttpErrorResponse){
+            console.error(err.error.error.code);
+            if (err.error?.error?.code === 401){
+              /** code = 401 为密码错误，重新打开输入密码modal，重发请求 */
+              return this.openEmailPasswordInputModals('密码错误，请重新输入').pipe(
+                switchMap(result => {
+                  /** 如果填了密码，那就发送请求 */
+                  if (result){
+                    return this.crmEmailService.seed({
+                      customerId: this.customerId,
+                      from: sender.emailAddress,
+                      fromUserId: sender.id,
+                      to: this.receives,
+                      subject: this.form.value.subject,
+                      body: this.form.value.value,
+                      cc: this.cc,
+                      bcc: this.bcc,
+                      fromPassword: result,
+                      attachments: this.fileList.getValue().map(i => i.fileId)
+                    });
+                  }else {
+                    /** 没有的话那就直接完成这个observable结束操作 */
+                    return EMPTY;
+                  }
+                })
+              );
+            }
+          }
+        })
+      ))
     ).subscribe({
       next: v => {
         console.log(v);
       },
-      error: e => {
-        console.error(e);
+      error: err => {
+        console.error(err);
+        this.sending.next(false);
+      },
+      complete: () => {
+        this.sending.next(false);
       }
     });
   }
